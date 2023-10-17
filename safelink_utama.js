8d7afe3b6468e1e015d13a042781bf4672dd3932
healtywellbeing <147817407+healtywellbeing@users.noreply.github.com>
2023-10-17 19:19:49 +0700

    Add files via upload

--- safelink_utama.js
+++ safelink_utama.js
@@ -21,7 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="4"){xxx="p/convert.html"}a_to_ve[i].href="https://www.safeinslink.xyz/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="bmw-increased-its-ebit-margin-outlook.html"}a_to_ve[i].href="https://www.safeinslink.xyz/2023/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
